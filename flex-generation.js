95bc28670b427590d750aee0e5be23805da11561
leon <pradit.y@ku.th>
2021-12-15 18:46:20 +0700

    flex promotion messages.

--- flex-generation.js
+++ flex-generation.js
@@ -1,136 +1,158 @@
 var msg = require("./pre-promotion-payload.json");
 
-var bodyContent = [
-  {
-    type: "image",
-    url: "https://i.imgur.com/VrLnVgK.jpg",
-    size: "full",
-    aspectMode: "cover",
-    aspectRatio: "1:1",
-    gravity: "top",
-  },
-  {
+var bodyContent = {
+  type: "bubble",
+  direction: "ltr",
+  size: "kilo",
+  body: {
     type: "box",
     layout: "vertical",
+    spacing: "none",
+    margin: "none",
     contents: [
+      {
+        type: "image",
+        url: "https://i.imgur.com/VrLnVgK.jpg",
+        size: "full",
+        aspectMode: "cover",
+        aspectRatio: "1:1",
+        gravity: "top",
+      },
       {
         type: "box",
         layout: "vertical",
         contents: [
           {
-            type: "filler",
-          },
-          {
             type: "box",
-            layout: "baseline",
+            layout: "vertical",
             contents: [
               {
                 type: "filler",
               },
               {
-                type: "text",
-                text: "คลิกเลย",
-                color: "#ffffff",
-                flex: 0,
-                offsetTop: "-2px",
+                type: "box",
+                layout: "baseline",
+                contents: [
+                  {
+                    type: "filler",
+                  },
+                  {
+                    type: "text",
+                    text: "คลิกเลย",
+                    color: "#ffffff",
+                    flex: 0,
+                    offsetTop: "-2px",
+                  },
+                  {
+                    type: "filler",
+                  },
+                ],
+                spacing: "sm",
               },
               {
                 type: "filler",
               },
             ],
+            borderWidth: "1px",
+            cornerRadius: "20px",
             spacing: "sm",
-          },
-          {
-            type: "filler",
+            borderColor: "#A7A9ACAA",
+            margin: "xxl",
+            height: "40px",
+            width: "100px",
+            offsetTop: "10px",
+            offsetStart: "65px",
+            backgroundColor: "#A7A9ACAA",
+            action: {
+              type: "uri",
+              label: "action",
+              uri: "http://linecorp.com/",
+            },
           },
         ],
-        borderWidth: "1px",
-        cornerRadius: "20px",
-        spacing: "sm",
-        borderColor: "#A7A9ACAA",
-        margin: "xxl",
-        height: "40px",
-        width: "100px",
-        offsetTop: "10px",
-        offsetStart: "65px",
-        backgroundColor: "#A7A9ACAA",
-        action: {
-          type: "uri",
-          label: "action",
-          uri: "http://linecorp.com/",
-        },
+        position: "absolute",
+        offsetBottom: "0px",
+        offsetStart: "0px",
+        offsetEnd: "0px",
+        paddingAll: "20px",
+        paddingTop: "18px",
       },
     ],
-    position: "absolute",
-    offsetBottom: "0px",
-    offsetStart: "0px",
-    offsetEnd: "0px",
-    paddingAll: "20px",
-    paddingTop: "18px",
   },
-]; //flex promotion
+}; //flex promotion
 
-var item = msg.messages;
+// var item = msg.messages;
 var imageUrlPromotion = [];
 
-if (item[0].type === "flex") {
-  console.log("flex");
-  var card = item[0].contents.contents;
-  for (let index = 0; index < card.length; index++) {
-    if (card[index].body.contents[1].text === "promotion") {
-      imageUrlPromotion.push(card[index].hero.url);
-      card[index].body.contents = bodyContent;
-      console.log(card[index].body.contents);
-      console.log("--------------------------------");
-      card[index].body.paddingAll = "0px";
+// if (item[0].type === "flex") {
+//   console.log("flex");
+//   var card = item[0].contents.contents;
+//   for (let index = 0; index < card.length; index++) {
+//     if (card[index].body.contents[1].text === "promotion") {
+//       imageUrlPromotion.push(card[index].hero.url);
+//       card[index].body.contents = bodyContent;
+//       console.log(card[index].body.contents);
+//       console.log("--------------------------------");
+//       card[index].body.paddingAll = "0px";
 
-      delete card[index].hero;
-      delete card[index].footer;
-    }
-  }
-  for (let index = 0; index < card.length; index++) {
-    card[index].body.contents[index].url = imageUrlPromotion[index];
-  }
-  msg.messages[0].contents.contents = card;
+//       delete card[index].hero;
+//       delete card[index].footer;
+//     }
+//   }
+//   for (let index = 0; index < card.length; index++) {
+//     card[index].body.contents[index].url = imageUrlPromotion[index];
+//   }
+//   msg.messages[0].contents.contents = card;
+// }
+
+// get in contents type bubble //
+
+var imageUrlArr = [];
+function getImageUrl(obj) {
+  obj.map((item) => {
+    imageUrlArr.push(item.hero.url);
+  });
 }
+getImageUrl(msg.messages[0].contents.contents);
+console.log(imageUrlArr);
 
-console.log(msg.messages[0].contents.contents[0].body.contents);
-console.log(msg.messages[0].contents.contents[1].body.contents);
-// console.log(msg.messages.contents.contents.body);
-// msg.messages = msg.messages.map((item) => {
-// //   item.altText = item.contents.contents[0].body.contents[0].text;
-//   if (item.type === "flex") {
-//         const card = item.contents.contents;
-//         for (let index = 0; index < card.length; index++) {
-//             if (card[index].body.contents[1].text === "promotion") {
-//                 card[index].body.contents = bodyContent;
-//                 card[index].body.contents[0].url = card[index].hero.url;
-//                 console.log(card[index].body.contents[0].url)
-//                 card[index].body.paddingAll = "0px";
+let tempContents = [];
+imageUrlArr.forEach((item, index) => {
+  var tempBodyContents = bodyContent;
+  tempContents.push(tempBodyContents);
+});
+// console.log(tempContents);
+const lastTempBodyContents = tempContents[0].body.contents.slice();
 
-//                 delete card[index].hero;
-//                 delete card[index].footer
-//             }
+imageUrlArr.forEach((item, index) => {
+  lastTempBodyContents.url = item;
+});
 
-//         }
-//         console.log(card)
-//     // item.contents.contents = item.contents.contents.map((card, index) => {
-//     //     console.log(card.body);
-//     //   if (card.body.contents[1].text === "promotion") {
-//     //     console.log("Is promotion");
-//     //     console.log(index);
-//     //     card.body.contents = bodyContent;
-//     //     bodyContent[0].url = card.hero.url;
-//     //     card.body.paddingAll = "0px";
-
-//     //     delete card.hero;
-//     //     delete card[.footer
-//     //   }
-//     //   return card;
-//     // });
-//   } else return item;
-//   return item;
-// });
+console.log(lastTempBodyContents[0].body.contents);
+console.log(lastTempBodyContents[1].body.contents);
+// let messagesObj = msg.messages[0];
+// msg.messages[0].contents.contents = msg.messages[0].contents.contents.map(
+//   (item, index) => {
+//     // var temp = msg.messages[0].contents.contents.map((item, index) => {
+//     // console.log(item);
+//     if (messagesObj.type === "flex") {
+//       if (item.body.contents[1].text === "promotion") {
+//         if (item.hero) imageUrlPromotion.push(item.hero.url);
+//         item = [].slice();
+//         // bodyContent.body.contents.slice();
+//         bodyContent.body.contents[0].url = imageUrlPromotion[index];
+//         // item.slice();
+//         item = bodyContent;
+//       }
+//     } else return item;
+//     // console.log(item.body.contents[0].url);
+//     return item;
+//   }
+// );
+// const card = msg.messages[0].contents.contents;
+// // console.log(imageUrlPromotion);
+// console.log(card[0].body);
+// console.log(card[1].body);
 msg.backup_payload = JSON.parse(JSON.stringify(msg));
 msg.backup_payload_zanroo = JSON.parse(JSON.stringify(msg));
 // return msg;
---
+++ send_data_cata.js
@@ -0,0 +1,13 @@
+const loanCollection =
+  msg.payload.hits.length > 0 &&
+  msg.payload.hits.map((item) => {
+    let card = item._source.item.json;
+    return {
+      productType: card.product_type.toString(),
+      productPicture: card.icon_green,
+      productDescTh: card.product_desc_th,
+      productDescEn: card.product_desc_en,
+    };
+  });
+msg.loanCollection = loanCollection;
+return msg;
