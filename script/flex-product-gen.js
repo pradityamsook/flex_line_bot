61437bdc9126b3dd616fe9ad10c84e4d71f17e74
leon <pradit.y@ku.th>
2021-12-23 16:12:41 +0700

    flex product class update.

---
+++ script/flex-product-gen.js
@@ -0,0 +1,275 @@
+node.warn(msg.payload);
+var getData = msg.payload.messages[0].text.split("|");
+getData.pop();
+node.warn(getData.length);
+node.warn(getData);
+// const preContents = msg.payload.messages[0].contents.contents;
+let newData = [];
+
+let bodyContent = {
+  type: "bubble",
+  header: {
+    type: "box",
+    layout: "vertical",
+    contents: [
+      {
+        type: "image",
+        url: "https://i.imgur.com/zGvnyzi.png",
+        aspectMode: "cover",
+        position: "absolute",
+        size: "300px",
+        aspectRatio: "4:1",
+      },
+      {
+        type: "text",
+        text: "กลุ่มซีเมนต์โครงสร้าง",
+        align: "center",
+        gravity: "center",
+        color: "#FFFFFF",
+      },
+    ],
+    height: "40px",
+    backgroundColor: "#CD242B",
+    justifyContent: "center",
+    alignItems: "center",
+    background: {
+      type: "linearGradient",
+      angle: "0deg",
+      startColor: "#CD242B",
+      endColor: "#7D1416",
+    },
+  },
+  body: {
+    type: "box",
+    layout: "vertical",
+    contents: [
+      {
+        type: "box",
+        layout: "baseline",
+        contents: [
+          {
+            type: "icon",
+            url: "https://i.imgur.com/pvinl1r.png",
+            aspectRatio: "1:1",
+            size: "40px",
+            offsetTop: "13px",
+          },
+          {
+            type: "text",
+            text: "ปูนงานโครงสร้าง เอสซีจี สูตรไฮบริด (ปูนซีเมนต์ถุง 50 กก.)",
+            align: "center",
+            gravity: "center",
+            adjustMode: "shrink-to-fit",
+            position: "relative",
+            offsetBottom: "15px",
+            size: "13px",
+          },
+        ],
+        spacing: "sm",
+        height: "60px",
+        alignItems: "center",
+        justifyContent: "flex-start",
+      },
+      {
+        type: "separator",
+      },
+      {
+        type: "box",
+        layout: "baseline",
+        contents: [
+          {
+            type: "icon",
+            url: "https://i.imgur.com/pvinl1r.png",
+            aspectRatio: "1:1",
+            size: "40px",
+            offsetTop: "13px",
+          },
+          {
+            type: "text",
+            text: "ปูนงานโครงสร้าง เอสซีจี สูตรไฮบริด (ปูนซีเมนต์ถุง 50 กก.)",
+            align: "center",
+            gravity: "center",
+            adjustMode: "shrink-to-fit",
+            position: "relative",
+            offsetBottom: "15px",
+            size: "13px",
+          },
+        ],
+        spacing: "sm",
+        height: "60px",
+        alignItems: "center",
+        justifyContent: "flex-start",
+      },
+      {
+        type: "separator",
+      },
+      {
+        type: "box",
+        layout: "baseline",
+        contents: [
+          {
+            type: "icon",
+            url: "https://i.imgur.com/pvinl1r.png",
+            aspectRatio: "1:1",
+            size: "40px",
+            offsetTop: "13px",
+          },
+          {
+            type: "text",
+            text: "ปูนงานโครงสร้าง เอสซีจี สูตรไฮบริด (ปูนซีเมนต์ถุง 50 กก.)",
+            align: "center",
+            gravity: "center",
+            adjustMode: "shrink-to-fit",
+            position: "relative",
+            offsetBottom: "15px",
+            size: "13px",
+          },
+        ],
+        spacing: "sm",
+        height: "60px",
+        alignItems: "center",
+        justifyContent: "flex-start",
+      },
+      {
+        type: "separator",
+      },
+      {
+        type: "box",
+        layout: "baseline",
+        contents: [
+          {
+            type: "icon",
+            url: "https://i.imgur.com/pvinl1r.png",
+            aspectRatio: "1:1",
+            size: "40px",
+            offsetTop: "13px",
+          },
+          {
+            type: "text",
+            text: "ปูนงานโครงสร้าง เอสซีจี สูตรไฮบริด (ปูนซีเมนต์ถุง 50 กก.)",
+            align: "center",
+            gravity: "center",
+            adjustMode: "shrink-to-fit",
+            position: "relative",
+            offsetBottom: "15px",
+            size: "13px",
+          },
+        ],
+        spacing: "sm",
+        height: "60px",
+        alignItems: "center",
+        justifyContent: "flex-start",
+      },
+      {
+        type: "separator",
+      },
+      {
+        type: "box",
+        layout: "baseline",
+        contents: [
+          {
+            type: "icon",
+            url: "https://i.imgur.com/pvinl1r.png",
+            aspectRatio: "1:1",
+            size: "40px",
+            offsetTop: "13px",
+          },
+          {
+            type: "text",
+            text: "ปูนงานโครงสร้าง เอสซีจี สูตรไฮบริด (ปูนซีเมนต์ถุง 50 กก.)",
+            align: "center",
+            gravity: "center",
+            adjustMode: "shrink-to-fit",
+            position: "relative",
+            offsetBottom: "15px",
+            size: "13px",
+          },
+        ],
+        spacing: "sm",
+        height: "60px",
+        alignItems: "center",
+        justifyContent: "flex-start",
+      },
+      {
+        type: "separator",
+      },
+      {
+        type: "box",
+        layout: "baseline",
+        contents: [
+          {
+            type: "icon",
+            url: "https://i.imgur.com/pvinl1r.png",
+            aspectRatio: "1:1",
+            size: "40px",
+            offsetTop: "13px",
+          },
+          {
+            type: "text",
+            text: "ปูนงานโครงสร้าง เอสซีจี สูตรไฮบริด (ปูนซีเมนต์ถุง 50 กก.)",
+            align: "center",
+            gravity: "center",
+            adjustMode: "shrink-to-fit",
+            position: "relative",
+            offsetBottom: "15px",
+            size: "13px",
+          },
+        ],
+        spacing: "sm",
+        height: "60px",
+        alignItems: "center",
+        justifyContent: "flex-start",
+      },
+      {
+        type: "separator",
+      },
+    ],
+  },
+  footer: {
+    type: "box",
+    layout: "vertical",
+    contents: [
+      {
+        type: "button",
+        action: {
+          type: "uri",
+          label: "ชมสินค้ากลุ่มนี้เพิ่มเติม คลิก!",
+          uri: "http://linecorp.com/",
+        },
+        style: "primary",
+        color: "#D61F26",
+      },
+    ],
+    borderWidth: "8px",
+    height: "90px",
+    cornerRadius: "8px",
+  },
+  styles: {
+    header: {
+      separator: true,
+    },
+  },
+};
+
+//   bodyContent.footer.contents[0].action.uri = v.hero.url;
+bodyContent.header.contents[1].text = getData[3];
+
+//   if (!v.body.contents[1].text) {
+//       node.warn("test")
+//       bodyContent.body.contents.text = "";
+//   } else {
+//       bodyContent.body.contents[2].text = v.body.contents[1].text;
+//   }
+let contents = {
+  type: "carousel",
+  contenst: newData,
+};
+newData.push(bodyContent);
+msg.payload.messages[0].type = "flex";
+msg.payload.messages[0].altText = "Products";
+msg.payload.messages[0].contents = contents;
+
+delete msg.payload.messages[0].text;
+msg.backup_payload = msg.payload;
+node.warn(msg.payload);
+return msg;
--- script/product-card-form.js
+++ script/product-card-form.js
@@ -47,3 +47,36 @@ console.log();
 // };
 
 // return msg;
+
+node.warn(msg);
+let data = JSON.parse(msg.payload);
+node.warn(data);
+// msg.payload = Buffer.from(data).toString("base64");
+
+var formatData = "";
+for (let index = 0; index < data.data.length; index++) {
+  for (let index = 0; index < datas.data.length; index++) {
+    var carousels = {
+      label: datas.data[index].promotion_name,
+      subtitle: datas.data[index].details,
+      img: datas.data[index],
+      btns: "รายละเอียด",
+      link: datas.data[index].promotion_page,
+    };
+    items.push(carousels);
+  }
+  formatData += `${data.data[index].priority}|${data.data[index].class_code}|${data.data[index].class_image}|${data.data[index].class_url}|`;
+  for (
+    let indexProd = 0;
+    indexProd < data.data[index].products.length;
+    indexProd++
+  ) {
+    formatData += `${data.data[index].products[indexProd].product_code}|${data.data[index].products[indexProd].product_name}|${data.data[index].products[indexProd].product_type}|${data.data[index].products[indexProd].product_barcode}|${data.data[index].products[indexProd].product_image}|${data.data[index].products[indexProd].product_url}|${data.data[index].products[indexProd].product_priority}`;
+    formatData += "|";
+  }
+}
+
+msg.payload = formatData;
+msg.payload.flexType = "Products";
+
+return msg;
