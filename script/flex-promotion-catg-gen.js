4d6a1ee978ecb8c4294016ea284135460baaacb0
Pradit Yamsook <pradit.y@ku.th>
2021-12-29 17:41:10 +0700

    Update get content in body flex messages of catalogue.

--- script/flex-promotion-catg-gen.js
+++ script/flex-promotion-catg-gen.js
@@ -1,10 +1,10 @@
 node.warn(msg.payload);
 const preContents = msg.payload.messages[0].contents.contents;
-var setDataBody =
-  msg.payload.messages[0].contents.contents[0].body.contents[1].text.split("|");
 let newData = [];
 
-preContents.map((v) => {
+preContents.map((v, index) => {
+  let setDataBody = v.body.contents[1].text.split("|");
+  node.warn(setDataBody.length);
   let bodyContent = {
     type: "bubble",
     body: {
@@ -67,7 +67,6 @@ preContents.map((v) => {
               size: "12px",
               margin: "7.09091px",
             },
-            {},
           ],
           paddingAll: "5px",
           offsetBottom: "10px",
@@ -112,21 +111,121 @@ preContents.map((v) => {
     },
   };
 
-  bodyContent.footer.contents[0].action.uri = v.hero.url; //set url image;
-  bodyContent.body.contents[0].url = v.hero.url; //set url button
-  bodyContent.body.contents[4].contents[0].text = setDataBody[2]; //set valid date;
+  let lastBodyContent = {
+    type: "bubble",
+    body: {
+      type: "box",
+      layout: "vertical",
+      contents: [
+        {
+          type: "image",
+          url: "https://i.imgur.com/VrLnVgK.jpg",
+          size: "full",
+          aspectMode: "cover",
+          aspectRatio: "1:1",
+          gravity: "top",
+        },
+        {
+          type: "box",
+          layout: "vertical",
+          contents: [
+            {
+              type: "text",
+              text: "โปรโมชั่นอื่น ๆ",
+              gravity: "center",
+              align: "center",
+              weight: "bold",
+              size: "14px",
+              style: "normal",
+            },
+          ],
+          paddingTop: "50px",
+        },
+      ],
+      paddingAll: "0px",
+    },
+    footer: {
+      type: "box",
+      layout: "vertical",
+      contents: [
+        {
+          type: "button",
+          action: {
+            type: "uri",
+            label: "action",
+            uri: "http://linecorp.com/",
+          },
+          gravity: "center",
+          adjustMode: "shrink-to-fit",
+          style: "primary",
+          color: "#ED1C24",
+        },
+      ],
+      backgroundColor: "#FFFFFF",
+      height: "120px",
+      position: "relative",
+      offsetBottom: "10px",
+      justifyContent: "center",
+    },
+  };
 
-  node.warn(v.body.contents[1].text);
-  if (!v.body.contents[1].text) {
-    node.warn("test");
-    bodyContent.body.contents.text = "";
-  } else {
-    bodyContent.body.contents[2].contents[0].text = v.body.contents[1].text;
+  let footerContent = {
+    footer: {
+      type: "box",
+      layout: "vertical",
+      contents: [
+        {
+          type: "button",
+          action: {
+            type: "uri",
+            label: "action",
+            uri: "http://linecorp.com/",
+          },
+          gravity: "center",
+          adjustMode: "shrink-to-fit",
+          style: "primary",
+          color: "#ED1C24",
+        },
+      ],
+      backgroundColor: "#FFFFFF",
+      height: "120px",
+      position: "relative",
+      offsetBottom: "10px",
+      justifyContent: "center",
+    },
+  };
+
+  if (v.footer.contents[0].action.label === "null") {
+    node.warn("footer");
+    footerContent.footer.contents.action.label =
+      v.footer.contents[1].action.label;
+    footerContent.footer.contents.action.uri = v.footer.contents[1].action.uri;
+    bodyContent.footer = footerContent;
   }
-  newData.push(...bodyContent);
+
+  if (index === preContents.length - 1) {
+    lastBodyContent.footer.contents[0].action.label =
+      v.footer.contents[1].action.label;
+    lastBodyContent.body.contents[0].url = v.hero.url;
+    newData.push(lastBodyContent);
+  } else {
+    bodyContent.footer.contents[0].action.uri = v.hero.url; //set url image;
+    bodyContent.body.contents[0].url = v.hero.url; //set url button
+    bodyContent.body.contents[1].contents[0].text = v.body.contents[0].text; // header name
+    if (setDataBody.length === 1)
+      bodyContent.body.contents[4].contents.text = v.body.contents[1].text;
+    else bodyContent.body.contents[4].contents[0].text = setDataBody[2]; //set valid date;
+
+    if (!v.body.contents[1].text) {
+      node.warn("test");
+      bodyContent.body.contents.text = "";
+    } else {
+      bodyContent.body.contents[2].contents[0].text = setDataBody[1];
+    }
+    newData.push(JSON.parse(JSON.stringify(bodyContent)));
+  }
 });
 msg.payload.messages[0].contents.contents = newData;
-// node.warn(msg.payload.messages);
 msg.backup_payload = JSON.parse(JSON.stringify(msg.payload));
 
 return msg;
