fe60a72494bbadb875f8b88ac300b9edfb0e0393
Pradit Yamsook <pradit.y@ku.th>
2022-01-12 21:43:46 +0700

    add function "toThaiDateString"

--- script/flex-product-keyword-gen.js
+++ script/flex-product-keyword-gen.js
@@ -1,4 +1,33 @@
 node.warn(msg.payload);
+
+// format to Thai data
+function toThaiDateString(date) {
+  let monthNames = [
+    "ม.ค.",
+    "ก.พ.",
+    "มี.ค.",
+    "ม.ย.",
+    "พ.ค.",
+    "มิ.ย.",
+    "ก.ค.",
+    "ส.ค.",
+    "ก.ย.",
+    "ต.ค.",
+    "พ.ย.",
+    "ธ.ค.",
+  ];
+
+  let year = date.getFullYear() + 543;
+  let month = monthNames[date.getMonth()];
+  let numOfDay = date.getDate();
+
+  let hour = date.getHours().toString().padStart(2, "0");
+  let minutes = date.getMinutes().toString().padStart(2, "0");
+  let second = date.getSeconds().toString().padStart(2, "0");
+
+  return `${numOfDay} ${month} ${year}`;
+}
+
 const preContents = msg.payload.messages[0].contents.contents;
 let newData = [];
 
