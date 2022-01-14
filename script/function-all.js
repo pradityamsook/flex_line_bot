22b386620d7409694c4ff56f6337e176e58a6a0a
Pradit Yamsook <pradit.y@ku.th>
2022-01-14 13:50:43 +0700

    add func toThaiDateString in to "function-all.js"

--- script/function-all.js
+++ script/function-all.js
@@ -1,7 +1,41 @@
-/** function format money to Thai bath*/
+/** function format money to Thai bath
+ * e.g. 1996 -> ฿1,996
+ */
 function formatMoneyTHB(money) {
   return money.toLocaleString("th-TH", {
     style: "currency",
     currency: "THB",
   });
+}
+
+/**function format date to Thai data
+ * but years reference from A.D. (e.g. 2022)
+ */
+// format to Thai data
+function toThaiDateString(date) {
+  let monthNames = [
+    "ม.ค.",
+    "ก.พ.",
+    "มี.ค.",
+    "ม.ย.",
+    "พ.ค.",
+    "มิ.ย.",
+    "ก.ค.",
+    "ส.ค.",
+    "ก.ย.",
+    "ต.ค.",
+    "พ.ย.",
+    "ธ.ค.",
+  ];
+
+  // let year = date.getFullYear() + 543; //uncomment if you want to convert to bhuddhist's year.
+  let year = date.getFullYear();
+  let month = monthNames[date.getMonth()];
+  let numOfDay = date.getDate();
+
+  let hour = date.getHours().toString().padStart(2, "0");
+  let minutes = date.getMinutes().toString().padStart(2, "0");
+  let second = date.getSeconds().toString().padStart(2, "0");
+
+  return `${numOfDay} ${month} ${year}`;
 }
