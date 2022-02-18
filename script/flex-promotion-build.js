ba1e5d62c3ec555948e88290ffb1d230e9542232
Pradit Yamsook <pradit.y@ku.th>
2022-02-18 14:43:13 +0700

    add header pre-contents.

--- script/flex-promotion-build.js
+++ script/flex-promotion-build.js
@@ -4,48 +4,50 @@ node.warn(datas);
 // node.warn(promotionData);
 
 const items = [];
-var valid_from;
-var valid_date;
+let valid_from;
+let valid_date;
+let countPromotionTwoButton = 0;
+let formatData = "pp promotion|";
+
+datas.data.forEach((element) => {
+    if (element.hasOwnProperty("promotion_products")) countPromotionTwoButton += 1;
+});
+node.warn(countPromotionTwoButton);
 for (let index = 0; index <= datas.data.length; index++) {
-  // valid_from = formatDate(datas.data[index].valid_from);
-  // valid_date = `${valid_from[0]} - ${formatDate(datas.data[index].valid_to)}`;
-  if (index === datas.data.length) {
-    var carouselsPromotion = {
-      label: promotionData.name,
-      subtitle: promotionData.detail,
-      img: promotionData.image,
-      btns: "รายละเอียด",
-      link: promotionData.url,
-    };
-    items.push(carouselsPromotion);
-  } else {
-    var promotionURL = "|";
-    if (datas.data[index].hasOwnProperty("promotion_page"))
-      promotionURL += `${datas.data[index].promotion_page}|`;
-    if (datas.data[index].hasOwnProperty("promotion_products"))
-      promotionURL += `${datas.data[index].promotion_products}|`;
-    var carouselsCard = {
-      label: datas.data[index].promotion_name,
-      subtitle: `${datas.data[index].promotion_description}|${valid_date}|${promotionURL}`,
-      img: encodeURI(datas.data[index].promotion_banner),
-      btns: "รายละเอียด",
-      link: datas.data[index].promotion_page,
-    };
-    items.push(carouselsCard);
-  }
+    if (index === datas.data.length) {
+        var carouselsPromotion = {
+            label: promotionData.name,
+            subtitle: `${promotionData.detail}|${promotionData.url}`,
+            img: promotionData.image,
+            btns: "รายละเอียด",
+            link: promotionData.url,
+        };
+        carouselsPromotion.subtitle = formatData + carouselsPromotion.subtitle;
+        items.push(carouselsPromotion);
+    } else {
+        var carouselsCard = {
+            label: datas.data[index].promotion_name,
+            subtitle: `Description:${datas.data[index].promotion_description}|Valid date:${valid_date}|Promotion page,${datas.data[index].promotion_page}|Count button:${countPromotionTwoButton}`,
+            img: encodeURI(datas.data[index].promotion_banner),
+            btns: "รายละเอียด",
+            link: datas.data[index].promotion_page,
+        };
+        carouselsCard.subtitle = formatData + carouselsCard.subtitle;
+        items.push(carouselsCard);
+    }
 }
 
 // node.warn(items);
 
 msg.payload = {
-  info: {
-    info_format: "items",
-    square_image: false,
-    items: items,
-  },
-  config: {
-    combine_output: false,
-  },
+    info: {
+        info_format: "items",
+        square_image: false,
+        items: items,
+    },
+    config: {
+        combine_output: false,
+    },
 };
 
 // node.warn(msg)
