c7c99c92b03da7ec468ca34ccd6d7381b4c93a2b
Pradit Yamsook <pradit.y@ku.th>
2022-02-18 14:43:43 +0700

    add entity configuration last card.

--- script/product-card-form.js
+++ script/product-card-form.js
@@ -1,89 +1,34 @@
-const data = require("./../json/product-api.json");
-
-var dayjs = require("dayjs");
-
-// let data = JSON.parse(msg.payload);
-// node.warn(data);
-
-const item = [];
-var formatData = "";
-for (let index = 0; index < data.data.length; index++) {
-  formatData += `
-    ${data.data[index].priority}|
-    ${data.data[index].class_code}|
-    ${data.data[index].class_image}|
-    ${data.data[index].class_url}|
-  `;
-  for (
-    let indexProd = 0;
-    indexProd < data.data[index].products.length;
-    indexProd++
-  ) {
-    var product_image = data.data[index].products[indexProd].product_image;
-    product_image.toString() != "null"
-      ? (product_image = product_image)
-      : (product_image = "https://imgur.com/VCMGiHY.png");
-
-    formatData += `
-        ${data.data[index].products[indexProd].product_code}|
-        ${data.data[index].products[indexProd].product_name}|
-        ${data.data[index].products[indexProd].product_type}|
-        ${data.data[index].products[indexProd].product_barcode}|
-        ${product_image}|
-        ${data.data[index].products[indexProd].product_url}|
-        ${data.data[index].products[indexProd].product_priority}
-    `;
-    formatData += "|";
-    formatData += "end_list";
-  }
-  formatData += "end_card|";
-}
-var splitDataLayer0 = formatData.split("Products");
-console.log();
-// node.warn(item);
-
-// msg.payload = {
-//   info: {
-//     info_format: "items",
-//     square_image: false,
-//     items: item,
-//   },
-//   config: {
-//     combine_output: false,
-//   },
-// };
-
-// return msg;
-
-node.warn(msg);
-let data = JSON.parse(msg.payload);
-node.warn(data);
-// msg.payload = Buffer.from(data).toString("base64");
-
-var formatData = "";
-for (let index = 0; index < data.data.length; index++) {
-  for (let index = 0; index < datas.data.length; index++) {
-    var carousels = {
-      label: datas.data[index].promotion_name,
-      subtitle: datas.data[index].details,
-      img: datas.data[index],
-      btns: "รายละเอียด",
-      link: datas.data[index].promotion_page,
+if (msg.statusCode == 200) {
+    let data = msg.data;
+    let lastCard = msg.payload.hits[0]._source.item.json;
+    const item = [];
+    let formatData = "Products|";
+    for (let index = 0; index < data.data.length; index++) {
+        if (data.data[index].products.length !== 0) {
+            formatData += `start|${data.data[index].priority}|${data.data[index].class_name}|${data.data[index].class_image}|${encodeURI(
+                data.data[index].class_url,
+            )}|`;
+            for (let indexProd = 0; indexProd < 5; indexProd++) {
+                var product_image = data.data[index].products[indexProd].product_image;
+                product_image !== null ? (product_image = product_image) : (product_image = "https://imgur.com/VCMGiHY.png");
+                formatData += `${data.data[index].products[indexProd].product_code}|${data.data[index].products[indexProd].product_name}|${
+                    data.data[index].products[indexProd].product_type
+                }|${data.data[index].products[indexProd].product_barcode}|${encodeURI(product_image)}|${
+                    data.data[index].products[indexProd].product_url
+                }|${data.data[index].products[indexProd].product_priority}`;
+                formatData += "|";
+                formatData += "end_list|";
+            }
+            formatData += "end_card|";
+        }
+    }
+    formatData += `last_card|${lastCard.Details}|${lastCard.Button}|${lastCard.Image}|${lastCard.URL}`;
+    // node.warn(formatData.split("|"));
+    msg.payload = {
+        status: "success",
+        data: formatData,
     };
-    items.push(carousels);
-  }
-  formatData += `${data.data[index].priority}|${data.data[index].class_code}|${data.data[index].class_image}|${data.data[index].class_url}|`;
-  for (
-    let indexProd = 0;
-    indexProd < data.data[index].products.length;
-    indexProd++
-  ) {
-    formatData += `${data.data[index].products[indexProd].product_code}|${data.data[index].products[indexProd].product_name}|${data.data[index].products[indexProd].product_type}|${data.data[index].products[indexProd].product_barcode}|${data.data[index].products[indexProd].product_image}|${data.data[index].products[indexProd].product_url}|${data.data[index].products[indexProd].product_priority}`;
-    formatData += "|";
-  }
+    return msg;
+} else {
+    return [null, msg];
 }
-
-msg.payload = formatData;
-msg.payload.flexType = "Products";
-
-return msg;
